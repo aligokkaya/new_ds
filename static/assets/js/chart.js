c34e3903f6b2d910461f04a20c79a2f6f8f76bc9
aligokkaya <ali.gkky196@gmail.com>
2022-05-27 20:58:15 +0300

    update

---
+++ static/assets/js/chart.js
@@ -0,0 +1,269 @@
+
+    $(function() {
+    /* ChartJS
+    * -------
+    * Data and config for chartjs
+    */
+    'use strict';
+    var data = {
+        labels: ['2022-02-20', '2022-02-12', '2022-01-17', '2021-11-28', '2021-11-07', '2021-10-30', '2021-10-04', '2021-08-19', '2021-07-18', '2021-04-29'],
+        datasets: [{
+        label: '# of Votes',
+        data: [3, 1, 1, 1, 3, 1, 1, 1, 1, 1],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            
+        ],
+        borderWidth: 1,
+        fill: false
+        }]
+    };
+    var data_bar = {
+        labels: ['2022-02', '2022-01', '2021-11', '2021-10', '2021-08', '2021-07', '2021-04'],
+        datasets: [{
+        label: '# of Votes',
+        data: [4, 1, 4, 2, 1, 1, 1],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            
+        ],
+        borderWidth: 1,
+        fill: false
+        }]
+    };
+
+    var options = {
+        scales: {
+        yAxes: [{
+            ticks: {
+            beginAtZero: true
+            },
+            gridLines: {
+            color: "rgba(204, 204, 204,0.1)"
+            }
+        }],
+        xAxes: [{
+            gridLines: {
+            color: "rgba(204, 204, 204,0.1)"
+            }
+        }]
+        },
+        legend: {
+        display: false
+        },
+        elements: {
+        point: {
+            radius: 0
+        }
+        }
+    };
+    var doughnutPieData = {
+        datasets: [{
+        data: [6, 4, 4],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 159, 64, 0.5)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        }],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: ['Telephone Detection', 'Sleep Detection', 'Cigarette Detection']
+    };
+
+    var doughnutPieOptions = {
+        responsive: true,
+        animation: {
+        animateScale: true,
+        animateRotate: true
+        }
+    };
+
+    var areaData = {
+        labels: [1.0],
+        datasets: [{
+        label: '# of Votes',
+        data: [14],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1,
+        fill: true, // 3: no fill
+        }]
+    };
+
+    var areaOptions = {
+        plugins: {
+        filler: {
+            propagate: true
+        }
+        },
+        scales: {
+        yAxes: [{
+            gridLines: {
+            color: "rgba(204, 204, 204,0.1)"
+            }
+        }],
+        xAxes: [{
+            gridLines: {
+            color: "rgba(204, 204, 204,0.1)"
+            }
+        }]
+        }
+    }
+
+    // Get context with jQuery - using jQuery's .get() method.
+    if ($("#barChart").length) {
+        var barChartCanvas = $("#barChart").get(0).getContext("2d");
+        // This will get the first returned node in the jQuery collection.
+        var barChart = new Chart(barChartCanvas, {
+        type: 'bar',
+        data: data_bar,
+        options: options
+        });
+    }
+
+    if ($("#lineChart").length) {
+        var lineChartCanvas = $("#lineChart").get(0).getContext("2d");
+        var lineChart = new Chart(lineChartCanvas, {
+        type: 'line',
+        data: data,
+        options: options
+        });
+    }
+
+    
+
+
+    if ($("#doughnutChart").length) {
+        var doughnutChartCanvas = $("#doughnutChart").get(0).getContext("2d");
+        var doughnutChart = new Chart(doughnutChartCanvas, {
+        type: 'doughnut',
+        data: doughnutPieData,
+        options: doughnutPieOptions
+        });
+    }
+
+    
+
+    if ($("#areaChart").length) {
+        var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+        var areaChart = new Chart(areaChartCanvas, {
+        type: 'line',
+        data: areaData,
+        options: areaOptions
+        });
+    }
+
+    
+    });
